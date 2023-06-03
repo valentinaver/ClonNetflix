06aa2518da28fdc957a21a6abdbc860d152c05d7
Valentina Vera <99610176+valentinaver@users.noreply.github.com>
2023-06-02 20:40:44 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+/*Derecha*/ 
+const carouselD = document.querySelector('.carousel');
+carousel.classList.remove('slide-left');
+carousel.classList.add('slide-right');
+
+/*Izquierda*/ 
+const carouselI = document.querySelector('.carousel');
+carousel.classList.remove('slide-right');
+carousel.classList.add('slide-left');
+
